bf0644498458236d6bdb4bdb1ad22a9baa61dc14
pathma2005 <pathmasri1372005@gmail.com>
2025-11-18 13:15:06 +0530

    some changes

--- module.js
+++ module.js
@@ -22,3 +22,50 @@ let stu1=new student("sri",20,"A");
 console.log(stu1.details());
 // 
 console.log(stu1.showgrade());
+// classess
+class person1{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+  bio(){
+      return "name: " + this.name +" age: " + this.age
+  }
+    }
+    const persons=new person1("sri",20)
+    console.log(persons.bio());
+    // class inheritance using extends keyword
+    class flower{
+        constructor(name,color){
+            this.name=name;
+            this.color=color;
+        }
+        flowers(){
+            return "name:"+this.name+"color:"+this.color;
+        }
+    }
+    class rose extends flower{
+        constructor(name,color,price){
+            super(name,color);
+            this.price=price;
+        }
+        showprice(){
+            return this.flowers()+"price:"+this.price;
+        }
+    }
+    let rose1=new rose("rose","red",20);
+    console.log(rose1.flowers());
+    console.log(rose1.showprice());
+// class using module exports
+class bike{
+    constructor(name,model){
+        this.name=name;
+        this.model=model;
+    }
+    bikes(){
+        return "name:"+this.name+"module:"+this.model;
+    }
+}
+module.export=bike;
+const bike1=new bike("ktm","duke");
+console.log(bike1.bikes());
